6a60d877407925e52945548426c89d5fa1ff444a
AndreSDS <andre_smiths@outlook.com>
2022-02-03 16:25:56 -0300

    Criando estrutura da apicação

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Blog News</title>
+      </Head>
+      <h1>Hello world</h1>
+    </>
+  );
+};
+
+export default Home;
